d7c3111ed9fa8502735ce6e0b26b75725069f252
sergiomtzs <sergiotomasr@gmail.com>
2026-04-11 15:18:14 +0200

    fix infoprojects

--- rohigarnez/backend/routes/projects.js
+++ rohigarnez/backend/routes/projects.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Projects = require('../models/Projects');
+const Projects = require('../models/InfoProjects');
 
 // GET for key
 
